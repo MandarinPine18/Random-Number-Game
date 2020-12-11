fc9d227742361584d77c6aa5d99fc65bb5349aca
ethanparab <59212466+ethanparab@users.noreply.github.com>
2020-12-11 11:59:14 -0600

    created game class

---
+++ RandomNumberGame.java
@@ -0,0 +1,25 @@
+public class RandomNumberGame {
+    private int min = Integer.MIN_VALUE;
+    private int max = Integer.MAX_VALUE;
+    private static boolean cheats = false; // change this to true if you want to enter cheat mode
+
+    public void promptRange() {
+
+    }
+
+    private void setMin(int min) {
+        if (max > min) {
+            this.min = min;
+        }
+    }
+
+    private void setMax(int max) {
+        if (max > min) {
+            this.max = max;
+        }
+    }
+
+    public static String getRules() {
+        return "This is the Random Number Guessing Game. A random number will be generated based on a range you provide. You must then guess the number. You have a limited number of guesses. Try your best and have fun!";
+    }
+}
