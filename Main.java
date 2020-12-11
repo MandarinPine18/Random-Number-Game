5a9c173bb5c90bf632cfd6e6ad9cbbbab6d314eb
Ethan Parab_2230216 <2230216@glenbard.org>
2020-12-11 17:18:51 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+/*Student name
+**Date
+**Period
+**Program name
+*/
+
+//imports
+
+
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    
+
+    //Welcome your user and ask for their name.
+
+
+
+
+    //Code a way to replay the game.
+    
+  }
+
+  //Create your Static game method here.
+}
