b72aa5d8b2d6ecddf1c382929c35c52df0647ce7
ethanparab <59212466+ethanparab@users.noreply.github.com>
2020-12-12 19:43:13 -0600

    not entirely sure what changed here

--- RandomNumberGame.java
+++ RandomNumberGame.java
@@ -1,26 +1,26 @@
 public class RandomNumberGame {
-    private int min = Integer.MIN_VALUE;
-    private int max = Integer.MAX_VALUE;
-    private static boolean cheats = false; // change this to true if you want to enter cheat mode
+	private int min = Integer.MIN_VALUE;
+	private int max = Integer.MAX_VALUE;
+	private static boolean cheats = false; // change this to true if you want to enter cheat mode
 
-    public RandomNumberGame(int[] range) {
-        setMin(range[0]);
-        setMax(range[1]);
-    }
+	public RandomNumberGame(int[] range) {
+		setMin(range[0]);
+		setMax(range[1]);
+	}
 
-    private void setMin(int min) {
-        if (max > min) {
-            this.min = min;
-        }
-    }
+	private void setMin(int min) {
+		if (max > min) {
+			this.min = min;
+		}
+	}
 
-    private void setMax(int max) {
-        if (max > min) {
-            this.max = max;
-        }
-    }
+	private void setMax(int max) {
+		if (max > min) {
+			this.max = max;
+		}
+	}
 
-    public static String getRules() {
-        return "This is the Random Number Guessing Game. A random number will be generated based on a range you provide. You must then guess the number. You have a limited number of guesses. Try your best and have fun!";
-    }
+	public static String getRules() {
+		return "This is the Random Number Guessing Game. A random number will be generated based on a range you provide. You must then guess the number. You have a limited number of guesses. Try your best and have fun!";
+	}
 }
