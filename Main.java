57ab4eb0ce3e166fe275fd148d54f9dd619d0a23
ethanparab <59212466+ethanparab@users.noreply.github.com>
2020-12-14 16:42:33 -0600

    added comments and altered iteration counting system

--- Main.java
+++ Main.java
@@ -10,23 +10,30 @@ import java.util.Scanner;
 
 
 class Main {
-    public static Scanner input = new Scanner(System.in);
+    public static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-
         //Welcome your user and ask for their name.
         String name = input("Enter your name: ");
         System.out.println("Welcome to the game, " + name);
         System.out.println(RandomNumberGame.getRules() + "\n");
 
+        // starting the game
         RandomNumberGame game = startGame();
 
+        // so the way I implemented the guess number system is in the game object. The actual values inside the object
+        // are private, but the object is able to give the main method a recommendation for how many guesses are a good
+        // number
         int guessLimit = game.recommendedGuesses();
         System.out.println("You have " + guessLimit + "guesses.");
 
-        for (int i = 0; i < guessLimit; i++) {
-            int guess = Integer.parseInt(input("Enter guess " + (i+1) + ": "));
+        // here I get into the guessing part of the game
+        // I start the count at one because, in any display portions, I always have to add one anyways. In order to offset that, <= is used instead of <
+        for (int i = 1; i <= guessLimit; i++) {
+            // get the guess from the user
+            int guess = Integer.parseInt(input("Enter guess " + i + ": "));
+
+            // GuessResult is an enumeration I made for this purpose, see the file for more information
             GuessResult result = game.processGuess(guess);
             System.out.println(guessToString(result, i) + "\n");
             if (result == GuessResult.CORRECT) {
@@ -57,12 +64,13 @@ class Main {
         return input.nextLine();
     }
 
+    // takes the GuessResult
     public static String guessToString(GuessResult result, int i) {
         String resultString = "Error in guess to string";
         switch (result) {
             case LOW -> resultString = "That guess was too low";
             case HIGH -> resultString = "That guess was too high";
-            case CORRECT -> resultString = "Congrats you correctly guessed the number in " + (i+1) + " guesses.";
+            case CORRECT -> resultString = "Congrats you correctly guessed the number in " + i + " guesses.";
         }
         return resultString;
     }
