87e7d344effb68adaef3be07fffbef74bb60a164
ethanparab <59212466+ethanparab@users.noreply.github.com>
2020-12-14 17:52:18 -0600

    added description

--- GuessResult.java
+++ GuessResult.java
@@ -1,3 +1,6 @@
+// These are the three different possible results of a guess. I thought Strings are too messy for this purpose and that
+// booleans are limited to only two states. ints are difficult to decipher and therefore error-prone. Therefore an
+// enumeration is the best option here
 public enum GuessResult {
 	LOW, CORRECT, HIGH
 }
