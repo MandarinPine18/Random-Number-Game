89af39763e4bb71abda1bc76058876d02b09eaa8
ethanparab <59212466+ethanparab@users.noreply.github.com>
2020-12-14 17:52:05 -0600

    added Command enum

---
+++ Command.java
@@ -0,0 +1,5 @@
+// This is a simple enumeration which allows me to have a unified command definition. In theory, I can also use this to
+// easily return commands from methods, but I have not faced the necessity yet
+public enum Command {
+	ERROR, RESTART, EXIT
+}
